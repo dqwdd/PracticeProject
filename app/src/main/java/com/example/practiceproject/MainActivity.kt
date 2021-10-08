006e838b01a564cc8c7599e9f1bf2dd06827ea2f
dqwdd <dqwdd@naver.com>
2021-10-08 20:46:11 +0900

    메니페스트에 3개 카메라, 저장소 퍼미션 추가 근데 갤러리에서 사진 불러오기는 되는데 카메라는 안됨 조정 중

--- app/src/main/java/com/example/practiceproject/MainActivity.kt
+++ app/src/main/java/com/example/practiceproject/MainActivity.kt
@@ -1,9 +1,30 @@
 package com.example.practiceproject
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ktx.BuildConfig
 import com.example.practiceproject.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
 
 class MainActivity : BaseActivity() {
 
@@ -11,6 +32,10 @@ class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
 
 
+    val CAMERA = arrayOf(Manifest.permission.CAMERA)
+    val STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    val CAMERA_CODE = 98
+    val STORAGE_CODE = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +45,189 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+
+        binding.imageView.setOnClickListener {
+
+
+
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setMessage("프로필 이미지를 변경하시겠습니까?")
+            alert.setNegativeButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+
+
+
+            })
+            alert.setPositiveButton("취소", null)
+            alert.show()
+
+
+
+
+        }
+
+
+        binding.camera.setOnClickListener {
+            CallCamera()
+        }
+
+
+        binding.btnAlbum.setOnClickListener() {
+            GetAlbum()
+        }
+
+
     }
 
     override fun setValues() {
     }
+
+
+
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            CAMERA_CODE -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "카메라 권한을 승인해 주세요.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+            STORAGE_CODE -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "저장소 권한을 승인해 주세요.", Toast.LENGTH_LONG).show()
+                        //finish() 앱을 종료함
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+    fun checkPermission(permissions: Array<out String>, type: Int): Boolean
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in permissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, permissions, type)
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+
+    fun CallCamera()
+    {
+        if (checkPermission(CAMERA, CAMERA_CODE) && checkPermission(STORAGE, STORAGE_CODE)) {
+            val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(itt, CAMERA_CODE)
+        }
+    }
+
+
+
+
+
+    fun saveFile(fileName: String, mimeType: String, bitmap: Bitmap): Uri?
+    {
+
+        var CV = ContentValues()
+        CV.put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+        CV.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            CV.put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, CV)
+
+
+        if (uri != null) {
+            var scriptor = contentResolver.openFileDescriptor(uri, "w")
+
+            if (scriptor != null) {
+                val fos = FileOutputStream(scriptor.fileDescriptor)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+                fos.close()
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    CV.clear()
+                    CV.put(MediaStore.Images.Media.IS_PENDING, 0)
+                    contentResolver.update(uri, CV, null, null)
+                }
+            }
+        }
+
+
+        return uri;
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_CODE -> {
+                    if (data?.extras?.get("data") != null) {
+                        val img = data?.extras?.get("data") as Bitmap
+                        val uri = saveFile(RandomFileName(), "image/jpg", img)
+                        binding.imageView.setImageURI(uri)
+                    }
+                }
+
+                STORAGE_CODE -> {
+                    val uri = data?.data
+                    binding.imageView.setImageURI(uri)
+                }
+            }
+        }
+    }
+
+
+
+
+    fun RandomFileName() : String
+    {
+        val fineName = SimpleDateFormat("yyyyMMddHHmmss").format(System.currentTimeMillis())
+        return fineName
+    }
+
+
+
+
+    fun GetAlbum()
+    {
+        if (checkPermission(STORAGE, STORAGE_CODE)) {
+            val itt = Intent(Intent.ACTION_PICK)
+            itt.type = MediaStore.Images.Media.CONTENT_TYPE
+            startActivityForResult(itt, STORAGE_CODE)
+        }
+    }
+
+
+
+
+
+
+
 
 
 }
